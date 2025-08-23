0334cfdacf1188fe48a531da2158e52b50029ebf
sumon-ahmed84 <sumonahmed275060@gmail.com>
2025-08-24 02:19:25 +0600

    added cashout section and togling

--- home.js
+++ home.js
@@ -1,4 +1,5 @@
 
+// add money functionality to the home page of the Payoo app
 
 const pin=1234;
 document.getElementById("add-money-btn").addEventListener("click",function(event){
@@ -24,4 +25,40 @@ document.getElementById("add-money-btn").addEventListener("click",function(event
     document.getElementById("availableBalance").innerText=totalNewAvailableBalance;
 })
 
+// cashout functionality
 
+document.getElementById("cashout-btn").addEventListener("click", function(event){
+    event.preventDefault();
+    const agentNumber=document.getElementById("agent-number").value;
+    const cashoutAmount=parseInt(document.getElementById("cashout-amount").value);
+    const cashoutPin=parseInt(document.getElementById("cashout-pin").value);
+    const availableBalanceElement=parseInt(document.getElementById("availableBalance").innerText);
+    if(agentNumber.length<11){
+        alert("Invalid agent number");
+        return;
+    }
+    if(cashoutAmount<=0){
+        alert("Invalid amount to cash out");
+        return;
+    }
+    if(cashoutPin!==pin){
+        alert("Invalid pin number");
+        return;
+    }
+    if(cashoutAmount>availableBalanceElement){
+        alert("Insufficient balance");
+        return;
+    }
+    const totalNewAvailableBalance=availableBalanceElement-cashoutAmount;
+    document.getElementById("availableBalance").innerText=totalNewAvailableBalance;
+})
+
+// toggle functionality
+document.getElementById("added-money").addEventListener("click",function(){
+    document.getElementById("cashout-section").style.display="none";
+    document.getElementById("add-section").style.display="block";
+})
+document.getElementById("cash-money").addEventListener("click",function(){
+    document.getElementById("add-section").style.display="none";
+    document.getElementById("cashout-section").style.display="block";
+})
